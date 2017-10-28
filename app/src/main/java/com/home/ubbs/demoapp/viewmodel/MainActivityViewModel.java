1be0f9159454b4a126b0953265f2ce509c0c266d
Udyatbhanu <udyatbhanu@gmail.com>
2017-10-28 10:58:00 -0700

    First Commit

---
+++ app/src/main/java/com/home/ubbs/demoapp/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,10 @@
+package com.home.ubbs.demoapp.viewmodel;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by udyatbhanu-mac on 10/28/17.
+ */
+
+public class MainActivityViewModel extends BaseObservable{
+}
